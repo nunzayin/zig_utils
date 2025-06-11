52c45bc8050d82fdc86d9cc215ecc73bd5817841
nz <64735893+nunzayin@users.noreply.github.com>
2025-06-11 22:59:27 +0300

    Improved echo
    Refactored echo so it performs a single write syscall while allocating
    the same amount of memory

--- echo/echo.zig
+++ echo/echo.zig
@@ -4,12 +4,25 @@ pub fn main() !void {
     var debugAllocator = std.heap.DebugAllocator(.{}).init;
     const allocator = debugAllocator.allocator();
     const stdout = std.io.getStdOut().writer();
-    const args = std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-    for (args[1..]) |arg| {
-        try stdout.writeAll(arg);
-        try stdout.writeAll(" ");
+
+    var argIter = try std.process.argsWithAllocator(allocator);
+    defer argIter.deinit();
+    var args = std.ArrayList(u8).init(allocator);
+    defer args.deinit();
+    _ = argIter.skip();
+    while (argIter.next()) |arg| {
+        try args.appendSlice(arg[0..arg.len]);
+        try args.append(" "[0]);
     }
-    try stdout.writeAll("\n");
-    try stdout.write(args);
+    args.items[args.items.len - 1] = "\n"[0];
+    try stdout.writeAll(args.items);
+
+    // const args = std.process.argsAlloc(allocator);
+    // defer std.process.argsFree(allocator, args);
+    // for (args[1..]) |arg| {
+    //     try stdout.writeAll(arg);
+    //     try stdout.writeAll(" ");
+    // }
+    // try stdout.writeAll("\n");
+    // try stdout.write(args);
 }
