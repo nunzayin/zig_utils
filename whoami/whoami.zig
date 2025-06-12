83d8305e52e4e3e2cdcf70bb0bfff5c9e963bb9e
nz <64735893+nunzayin@users.noreply.github.com>
2025-06-12 14:06:28 +0300

    Improved whoami a bit
    Tried my best not to use libc but pure zig variant drops a shit ton of
    read syscalls

--- whoami/whoami.zig
+++ whoami/whoami.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    try std.io.getStdOut().writer()
-        .writeAll(std.mem.span(std.c.getpwuid(std.os.linux.geteuid()).?.name.?));
+    try stdout.writeAll(std.mem.span(std.c.getpwuid(std.os.linux.geteuid()).?.name.?));
+    try stdout.writeAll("\n");
 }
